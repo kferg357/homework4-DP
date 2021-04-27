30c480aa0050a015e9ea22935b1652eb98171159
kferg357 <kennethferguson90@gmail.com>
2021-04-26 20:19:34 -0400

    first commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// var currentDay= null'
+//     lead = null;
+
+// var update = function () {
+//     date = moment(new Date())
+//     currentDay.html(date.format('dddd');  );
+// };
+
+var currentDay = moment();
+console.log(currentDay.format('dddd'));
